48a40a4b0f301ee2bc3dba0c495d6bd50690b477
Jonas Malaco <jonas@protocubo.io>
2017-10-11 05:27:40 -0300

    Lazy load all primitives
    This allows the library user to, for instance, add custom search paths
    to the local `neko.vm.Loader` before the call is made to
    `loadPrimitive`.

--- lib/mbedtls/Md5.hx
+++ lib/mbedtls/Md5.hx
@@ -7,7 +7,13 @@ import haxe.io.Bytes;
 MD5 Message Digest Algorithm
 */
 class Md5 {
-	static var _make:BytesData->BytesData = neko.Lib.load("mbedtls", "md5_make", 1);
+	static var _make(get,null):BytesData->BytesData;
+		static inline function get__make()
+		{
+			if (_make == null)
+				_make = neko.Lib.load("mbedtls", "md5_make", 1);
+			return _make;
+		}
 
 	public static function encode(s:String):String
 		return Bytes.ofData(_make(BytesData.ofString(s))).toHex();
--- lib/mbedtls/Sha1.hx
+++ lib/mbedtls/Sha1.hx
@@ -7,7 +7,13 @@ import haxe.io.Bytes;
 SHA-1 Cryptographic Hash Function
 */
 class Sha1 {
-	static var _make:BytesData->BytesData = neko.Lib.load("mbedtls", "sha1_make", 1);
+	static var _make(get,null):BytesData->BytesData;
+		static inline function get__make()
+		{
+			if (_make == null)
+				_make = neko.Lib.load("mbedtls", "sha1_make", 1);
+			return _make;
+		}
 
 	public static function encode(s:String):String
 		return Bytes.ofData(_make(BytesData.ofString(s))).toHex();
